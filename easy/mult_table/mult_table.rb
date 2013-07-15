e512a9bcb776696647c4d0a64ddbf09cd052dc00
Neil Hoff <neilhoff@gmail.com>
2013-07-14 23:05:22 -0500

    finished mult_table

--- easy/mult_table/mult_table.rb
+++ easy/mult_table/mult_table.rb
@@ -1,7 +1,16 @@
 require 'matrix'
-m_table = Matrix.build(12,12) {|row, col| row +1 * col +1}
+m_table = Matrix.build(12,12) {|row, col| (row+1) * (col+1)}
 
-m_table.each do |row|
-	p row.inspect
+count=0
+table = ""
+m_table.each do |num|
+	if count < 11
+		table << "#{num}".rjust(4)
+		count +=1
+	else
+		table << "#{num}".rjust(4) + "\n"
+		count = 0
+	end
 end
 
+puts table
